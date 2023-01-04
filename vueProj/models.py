b246a6b1e988da9d0d1acf8e83583419b87cef75
Liuqiang4Android <liuqiang44@live.cn>
2023-01-04 16:46:40 +0800

    创建model

--- vueProj/models.py
+++ vueProj/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    book_name = models.CharField(max_length=64)
+    add_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.book_name
+
+#只有两个字段，书名book_name和添加时间add_time。如果没有指定主键的话django会自动新增一个自增id作为主键
